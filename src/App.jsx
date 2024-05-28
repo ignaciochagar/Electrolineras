5666b22bc20cf212f8868d8ee7ab07a9e9efd5e9
ignaciochagar <you@example.com>
2024-05-28 23:36:25 +0200

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,28 @@ import './App.css';
 import Map from './components/Map'
 import Login from "./components/login/Login"; // Asegúrate de que la ruta del componente Login sea correcta
 
+import React from 'react';
+import Navbar from './components/navbar/navbar';
+
 function App() {
 
   return (
-    <>
-      <h1>Inicia Sesion</h1>
-      <Login /> {/* Renderiza el componente Login aquí */}
+    <div>
+      <Navbar />
+      <>
+        <h1>Inicia Sesion</h1>
+        <Login /> {/* Renderiza el componente Login aquí */}
 
-      <article className="map-outer">
+        <article className="map-outer">
           <Map
-          
+
             title="Camino de Santiago"
 
           />
-      </article>
-    </>
-  );
+        </article>
+      </>
+    </div> 
+      );
 }
 
-export default App;
+      export default App;
