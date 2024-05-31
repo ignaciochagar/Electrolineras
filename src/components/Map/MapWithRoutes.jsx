0538befa743eb315dfc96f2975c33534538b31c8
ignaciochagar <you@example.com>
2024-05-31 13:34:43 +0200

    Boton mostrar chinchetas

--- src/components/Map/MapWithRoutes.jsx
+++ src/components/Map/MapWithRoutes.jsx
@@ -5,17 +5,20 @@ import alojamientos from "../../Database/Alojamientos.json";
 import icono from "../../../public/img/greenMarker.png";
 
 const icon = new L.Icon({
-  iconUrl:  icono,
+  iconUrl: icono,
   iconSize: [32, 32],
   iconAnchor: [16, 0], // Point of the icon which will correspond to marker's location
   popupAnchor: [0, -12] // Point from which the popup should open relative to the iconAnchor
 });
+
 const MapWithRoutes = ({ coordinates, camino }) => {
   const [filteredAlojamientos, setFilteredAlojamientos] = useState([]);
+  const [showMarkers, setShowMarkers] = useState(true); // Estado para controlar la visibilidad de los marcadores
+
   console.log("el camino es", camino);
 
   useEffect(() => {
-    // Filter the alojamientos based on the selected camino
+    // Filtrar los alojamientos basados en el camino seleccionado
     const alojamientosChinchetas = alojamientos.filter(alojamiento => alojamiento.camino === camino);
 
     if (alojamientosChinchetas.length === 0) {
@@ -25,15 +28,23 @@ const MapWithRoutes = ({ coordinates, camino }) => {
     setFilteredAlojamientos(alojamientosChinchetas);
   }, [camino]);
 
+  // Función para alternar la visibilidad de los marcadores
+  const toggleMarkers = () => {
+    setShowMarkers(!showMarkers);
+  };
+
   return (
     <section className='map'>
+      <button onClick={toggleMarkers}>
+        {showMarkers ? 'Ocultar Chinchetas' : 'Mostrar Chinchetas'}
+      </button>
       <MapContainer center={[40.43731467230963, -3.689847347507708]} zoom={6} style={{ height: '550px', width: '1000px' }}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
         <Polyline positions={coordinates} color="blue" />
-        {filteredAlojamientos.map(alojamiento => (
+        {showMarkers && filteredAlojamientos.map(alojamiento => (
           <Marker
             key={alojamiento.Alojamiento_id}
             position={[alojamiento.latitud, alojamiento.longitud]}
@@ -54,3 +65,4 @@ const MapWithRoutes = ({ coordinates, camino }) => {
 };
 
 export default MapWithRoutes;
+
